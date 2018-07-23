2ed4f67a37fa81345f141a633820d9632ee9d73f
Francisco Hernandez <fhernandezj@uninorte.edu.co>
2018-07-23 11:42:48 -0500

    Initial routes implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+
+// Routes
+import { APP_ROUTING } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
